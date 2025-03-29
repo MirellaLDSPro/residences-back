760147d704f75bedd0245d14d073d10621d20340
MirellaLDS <mirella.lins@mercos.com>
2025-03-29 02:27:57 -0300

    router teste

--- api/contatos.js
+++ api/contatos.js
@@ -1,23 +1,8 @@
 const express = require('express');
-const app = express();
+const router = express.Router();
+const contatosController = require('../controllers/contatos');
 
-app.use(express.json());
+router.get('/', contatosController.listar);
+router.post('/', contatosController.criar);
 
-let contatos = [
-    { id: 1, nome: "João", telefone: "61-9999-9999" }
-];
-
-app.get('/api/contatos', (req, res) => {
-    res.json(contatos);
-});
-
-app.post('/api/contatos', (req, res) => {
-    const novoContato = {
-        id: contatos.length + 1,
-        ...req.body
-    };
-    contatos.push(novoContato);
-    res.status(201).json(novoContato);
-});
-
-module.exports = app;
+module.exports = router;
--- routes/contatos.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contatosController = require('../controllers/contatos');
-
-router.get('/', contatosController.listar);
-router.post('/', contatosController.criar);
-
-module.exports = router;
