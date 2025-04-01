b4fc02c8d5005c8017a4d46e31c7722a8576ca9f
Mirella Lins <mirella.lds@gmail.com>
2025-03-31 22:11:19 -0300

    teste

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -4,13 +4,26 @@ import { getFirestore } from 'firebase-admin/firestore';
 import { readFileSync } from 'fs';
 
 // Carrega as credenciais
-const serviceAccount = JSON.parse(
-  readFileSync(new URL('./service-account.json', import.meta.url))
-);
+// const serviceAccount = JSON.parse(
+//   readFileSync(new URL('./service-account.json', import.meta.url))
+// );
 
 // Inicializa o Admin SDK
 const adminApp = initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert({
+    type: process.env.FIREBASE_ADMIN_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    client_id: process.env.CLIENTE_ID,
+    client_id: "103836585568240066799",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40residence-back.iam.gserviceaccount.com",
+    universe_domain: "googleapis.com"
+  })
 });
 
 export const adminAuth = getAuth(adminApp);
