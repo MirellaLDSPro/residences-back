138f4a6583402651de940bd1ab15c85d4f057eee
Mirella Lins <mirella.lds@gmail.com>
2025-04-01 10:57:29 -0300

    Consumir chame vercel

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -18,10 +18,9 @@ const adminApp = initializeApp({
     type: 'service_account',
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
+    private_key: formatAdminKey(),
     client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
     client_id: process.env.CLIENTE_ID,
-    client_id: "103836585568240066799",
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
@@ -29,6 +28,13 @@ const adminApp = initializeApp({
     universe_domain: "googleapis.com"
   })
 });
+
+function formatAdminKey() {
+  const localKey = process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n') || '';
+  const prodKey = process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace('|||', '\n') || '';
+  
+  return process.env.NODE_ENV === 'development' ? localKey : prodKey?.replace('|||', /\\n/g) || '';
+}
 
 export const adminAuth = getAuth(adminApp);
 export const adminDb = getFirestore(adminApp);
