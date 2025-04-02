93baf03e5e6deab87ede0eee9361c811beea8ffe
Mirella Lins <mirella.lds@gmail.com>
2025-04-02 00:38:39 -0300

    jjuu

--- api/debug-env.js
+++ api/debug-env.js
@@ -3,7 +3,6 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    console.log(process.env.TESTE_KEY?.replaceAll("|||", '\\n'));
     res.json({
       keyExists: !!process.env.TESTE_KEY,
       firstChars: process.env.TESTE_KEY?.replaceAll("|||", '\\n')
